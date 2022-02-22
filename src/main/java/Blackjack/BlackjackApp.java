4590d4890bac2b53a919ee3cf96ce470c74835b0
Jens <jensesk@stud.ntnu.no>
2022-02-22 12:52:39 +0100

    version 1

--- src/main/java/Blackjack/BlackjackApp.java
+++ src/main/java/Blackjack/BlackjackApp.java
@@ -1,7 +1,10 @@
 package Blackjack;
 
 public class BlackjackApp {
+
+
     public static void main(String[] args) {
         System.out.println("hei");
+        System.out.println("hei2");
     }
 }
